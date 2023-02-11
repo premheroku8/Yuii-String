cc6b00ed78176207220ad9eb52ece0b343eab586
Prem Heroku <124359592+premheroku8@users.noreply.github.com>
2023-02-11 09:57:39 +0800

    Update env.py

--- env.py
+++ env.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "12861350").strip()
-API_HASH = os.getenv("API_HASH", "e1429ae7374edc95c986d652df783bf7").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "5619588117:AAGQVPs6LY3VpToYsORw49NbrpFtYl62Md8").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "postgres://yxghfmci:QkCv1t2hzOzHZz9J-my1xx9En3QgUvwC@ruby.db.elephantsql.com/yxghfmci").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/zennihhh")
+API_ID = os.getenv("API_ID", "").strip()
+API_HASH = os.getenv("API_HASH", "").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
+DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
+MUST_JOIN = os.getenv("MUST_JOIN", "")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
